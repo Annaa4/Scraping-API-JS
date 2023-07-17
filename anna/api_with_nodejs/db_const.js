292384972a752fe99391b8b228e17d8085e648c9
wade <waderawane873@gmail.com>
2023-07-17 07:57:33 +0000

    page chaussure

--- anna/api_with_nodejs/db_const.js
+++ anna/api_with_nodejs/db_const.js
@@ -1,8 +1,8 @@
-const USER="anna";
-const DB="data-api";
-const HOST="postgresql-134127-0.cloudclusters.net";
+const USER="rawane";
+const DB="data_api";
+const HOST="postgresql-136064-0.cloudclusters.net";
 const PASS="12345678";
-const PORT="19529";
+const PORT="19824";
 
 module.exports = {
     USER,
