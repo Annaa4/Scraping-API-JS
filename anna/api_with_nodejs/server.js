41f551e7d759f92f548b45bca194ece049e4a523
wade <waderawane873@gmail.com>
2023-07-24 08:00:12 +0000

    modif

--- anna/api_with_nodejs/server.js
+++ anna/api_with_nodejs/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const materielRoutes = require('./src/materiels/routes');
-const{adminAuth,userAuth}=require('./User/UserAuth/Auth')
+const{adminAuth,userAuth, logout}=require('./User/UserAuth/Auth')
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
@@ -36,6 +36,9 @@ app.set("view engine","ejs")
 app.get("/", (req, res) => res.render('index'));
 app.get("/ins", (req, res) => res.render('inscription'))
 app.get("/connect", (req, res) => res.render('connexion'))
+app.get("/chaussure", (req, res) => res.render('chaussure'))
+
+app.get("/logout", logout)
 
 app.get("/admin", adminAuth, (req, res) => res.send("Route Pour Admin"));
 app.get("/basic", userAuth, (req, res) => res.send("User Route"));
